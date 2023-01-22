c6a16ad9ff64fb7c5b161e3982e8ee62b74a637a
Athisankaran <athisankaranb@gmail.com>
2023-01-22 13:17:33 +0530

    csv finalised

--- Codes/Generator.py
+++ Codes/Generator.py
@@ -1,40 +1,54 @@
 import random
 import string
 import pandas as pd
+import os
 
 CredentialsYT = {}
+OldMailId = []
+OldPassword = []
+OldUserFirst = []
+OldUserLast = []
 GeneratedMailId = []
 GeneratedPassword = []
 UserFirst = []
 UserLast = []
 
-
 class Generate:
-
     def __init__(self, GenerateCount = 100):
         self.GenerateCount = GenerateCount
 
     def MailPassGenerator(self):
         j = 0
         for i in range(self.GenerateCount + j):
-            TempMailId = (''.join(random.choices(string.ascii_letters, k = 7))).lower() + "@gmail.com"
-            if((TempMailId not in GeneratedMailId)):
-                TempPassword = TempMailId[:3] + TempMailId[4:7] + "@" + "098"
+            TempMailId = (''.join(random.choices(string.ascii_letters, k = 14))).lower() + "@gmail.com"
+            if(TempMailId not in GeneratedMailId and TempMailId not in OldMailId):
+                TempPassword = TempMailId[:4] + TempMailId[10:14] + "@098"
                 TempUserFirst = TempMailId[:4]
-                TempUserLast = TempMailId[4:7]
+                TempUserLast = TempMailId[6:10]
                 GeneratedMailId.append(TempMailId)
                 GeneratedPassword.append(TempPassword)
                 UserFirst.append(TempUserFirst)
                 UserLast.append(TempUserLast)
-                CredentialsYT["MailId"] = GeneratedMailId
-                CredentialsYT["Password"] = GeneratedPassword
-                CredentialsYT["FirstName"] = UserFirst
-                CredentialsYT["LastName"] = UserLast
             else:
                 j += 1
+        CredentialsYT["MailId"] = OldMailId + GeneratedMailId
+        CredentialsYT["Password"] = OldPassword + GeneratedPassword
+        CredentialsYT["FirstName"] = OldUserFirst + UserFirst
+        CredentialsYT["LastName"] = OldUserLast + UserLast
         return CredentialsYT
+
+if(os.path.exists("../Credentials.csv")):
+    CredentialsDetails = pd.read_csv("../Credentials.csv")
+    for Mail in CredentialsDetails.MailId:
+        OldMailId.append(Mail)
+    for Pass in CredentialsDetails.Password:
+        OldPassword.append(Pass)
+    for FName in CredentialsDetails.FirstName:
+        OldUserFirst.append(FName)
+    for LName in CredentialsDetails.LastName:
+        OldUserLast.append(LName)
 
 GeneratorObject = Generate(5)
 print(GeneratorObject.MailPassGenerator())
 df = pd.DataFrame(CredentialsYT)
-df.to_csv("Credentials.csv", index = False, header = True)
+df.to_csv("../Credentials.csv", index = False, header = True)
